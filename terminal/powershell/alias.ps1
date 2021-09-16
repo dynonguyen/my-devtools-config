9495cb5cf22284fbc674b9af6a8fec7a9744c90e
Tuấn Nguyễn <tuannguyentn2504@gmail.com>
2021-09-16 21:35:27 +0700

    💥 Update powershell alias

--- terminal/powershell/alias.ps1
+++ terminal/powershell/alias.ps1
@@ -30,18 +30,36 @@ Function chocolist{ choco list --localonly }
 # open $profile with nodepad
 Function op{ notepad $PROFILE }
 # open file with notepad
-Function no{ notepad $args }
-
+Function no{ notepad $args[0] }
+# show all file
+Function lsa{ ls -Force }
 # --------- Project Manager ---------
-Function pdemo{ cd "C:\code\demo" }
 Function pcode{ cd "C:\code" }
 Function vimconf{ cd "C:\Users\Tuan\AppData\Local\nvim" }
 Function config{ cd "C:\terminal-config" }
 
 # --------- npm ---------
-Function ni{ npm install $args }
-Function nig{ npm install -g $args }
-Function nid{ npm install --save-dev $args }
+Function ni{ 
+  $cmd = "npm install"
+  for ( $i = 0; $i -lt $args.count; $i++ ) {
+    $cmd += ' ' + $args[$i]
+  } 
+  Invoke-Expression $cmd
+}
+Function nig{
+  $cmd = "npm install -g"
+  for ( $i = 0; $i -lt $args.count; $i++ ) {
+    $cmd += ' ' + $args[$i]
+  } 
+  Invoke-Expression $cmd
+}
+Function nid{
+  $cmd = "npm install --save-dev"
+  for ( $i = 0; $i -lt $args.count; $i++ ) {
+    $cmd += ' ' + $args[$i]
+  } 
+  Invoke-Expression $cmd
+}
 Function ns{ npm start }
 Function nt{ npm run test }
 Function nb{ npm run build }
@@ -54,10 +72,34 @@ Function yt{ yarn test }
 Function yb{ yarn build }
 Function yd{ yarn dev }
 Function ydp{ yarn deploy }
-Function ya{ yarn add $args }
-Function yag{ yarn add -g $args }
-Function yad{ yarn add --dev $args }
-Function yr{ yarn remove $args }
+Function ya{ 
+  $cmd = "yarn add"
+  for ( $i = 0; $i -lt $args.count; $i++ ) {
+    $cmd += ' ' + $args[$i]
+  } 
+  Invoke-Expression $cmd
+}
+Function yag{ 
+  $cmd = "yarn add -g"
+  for ( $i = 0; $i -lt $args.count; $i++ ) {
+    $cmd += ' ' + $args[$i]
+  } 
+  Invoke-Expression $cmd
+}
+Function yad{ 
+  $cmd = "yarn add --dev"
+  for ( $i = 0; $i -lt $args.count; $i++ ) {
+    $cmd += ' ' + $args[$i]
+  } 
+  Invoke-Expression $cmd
+}
+Function yr{ 
+  $cmd = "yarn remove"
+  for ( $i = 0; $i -lt $args.count; $i++ ) {
+    $cmd += ' ' + $args[$i]
+  } 
+  Invoke-Expression $cmd
+}
 
 # --------- Git, Github ---------
 Function gs{ git status }
@@ -67,6 +109,15 @@ Function gsh{ git show $args }
 Function gl{ git log }  
 Function gd{ git diff $args }
 Function ga{ git add $args }
+Function grsta{ git restore --staged $args }
+Function gaex{
+  git add .
+  $cmd = "git reset --"
+  for ( $i = 0; $i -lt $args.count; $i++ ) {
+    $cmd += ' ' + $args[$i]
+  } 
+  Invoke-Expression $cmd
+}
 Function gcm{ git commit -m $args }
 Function gca{ git commit --amend }
 Function gb{ git branch }
@@ -85,7 +136,6 @@ Function grs{ git reset --soft $args }
 Function grh{ git reset --hard $args }
 Function grm{ git reset --mixed $args }
 Function grb{ git rebase $args }
-
 
 # --------- Neovim ---------
 Function vi{ nvim $args }
