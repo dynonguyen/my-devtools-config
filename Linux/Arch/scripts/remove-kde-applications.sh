f7a436f1102793d0b76d3b42cafd741c55d72c0f
Tuan Nguyen <tuannguyentn2504@gmail.com>
2022-01-30 06:14:34 +0700

    Add pacman, AUR tutorial & Arch README

--- Linux/Arch/scripts/remove-kde-applications.sh
+++ Linux/Arch/scripts/remove-kde-applications.sh
@@ -1,4 +1,4 @@
 # script to remove unnecessary KDE applications
-sudo pacman -Rcns kate kde-games blinken kanagram khangman kde-education kde-graphics kontrast kde-dev-utils akonadiconsole cervisia kapptemplate kcachegrind kompare lokalize umbrello kmail kontact kcharselect ktimer libksieve korganizer kwrite kwrited knotes kteatime kfloppy kalarm kitinerary kmag vlc kmouth zanshin kmail-account-wizard plasma-browser-integration kget kopete
+sudo pacman -Rcns kate kde-games blinken kanagram khangman kde-education kde-graphics kontrast kde-dev-utils akonadiconsole cervisia kapptemplate kcachegrind kompare lokalize umbrello kmail kontact kcharselect ktimer libksieve korganizer kwrite kwrited knotes kteatime kfloppy kalarm kitinerary kmag vlc kmouth zanshin kmail-account-wizard plasma-browser-integration kget kopete mailimporter akonadi-notes 
 
 sudo pacman -Rcn kmailtransport konqueror
