40d343075314c690a9803a09809096cad8c13e13
Nguyen Le Anh Tuan <tuannguyentn2504@gmail.com>
2024-05-18 10:33:15 +0700

    Update

--- linux/zsh/my-alias.zsh
+++ linux/zsh/my-alias.zsh
@@ -45,8 +45,10 @@ function save_settings(){
 
 function sync_settings() {
   # Pull setting from github
+  currentPath=$(pwd)
   cd $CONFIG_PATH
   git pull
+  cd $currentPath
 
   # Neovim
   cp -rp $CONFIG_PATH/common/nvim/* $HOME/.config/nvim
--- macos/zsh/my-alias.zsh
+++ macos/zsh/my-alias.zsh
@@ -34,8 +34,10 @@ function save_settings(){
 
 function sync_settings() {
   # Pull setting from github
+  currentPath=$(pwd)
   cd $CONFIG_PATH
   git pull
+  cd $currentPath
 
   # Neovim
   cp -rp $CONFIG_PATH/common/nvim/* $HOME/.config/nvim
