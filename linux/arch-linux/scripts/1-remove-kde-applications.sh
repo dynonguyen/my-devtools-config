fec93a35fe4d09c17d2197d72c64f25a04c9a04b
TuanNLA5 <tuannla5@fpt.com>
2024-05-31 13:39:16 +0700

    Update

--- linux/arch-linux/scripts/1-remove-kde-applications.sh
+++
@@ -1,4 +0,0 @@
-# script to remove unnecessary KDE applications
-sudo pacman -Rcns kate kde-games blinken kanagram khangman kde-education kde-graphics kontrast kde-dev-utils akonadiconsole cervisia kapptemplate kcachegrind kompare lokalize umbrello kmail kontact kcharselect ktimer libksieve korganizer kwrited knotes kteatime kfloppy kalarm kitinerary kmag kmouth zanshin kmail-account-wizard plasma-browser-integration kget kopete mailimporter akonadi-notes yakuake
-
-sudo pacman -Rcn kmailtransport konqueror discover filelight juk kdf k3b
