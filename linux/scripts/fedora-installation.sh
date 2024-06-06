168b4586a90b6f266fc67673ce46c53842783d44
TuanNLA5 <tuannla5@fpt.com>
2024-06-06 11:04:29 +0700

    Update

--- linux/scripts/fedora-installation.sh
+++ linux/scripts/fedora-installation.sh
@@ -1,1 +1,19 @@
-dnf install vim neovim
+sudo dnf install dnf5 vim neovim zsh
+
+# Vscode
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+
+sudo dnf check-update
+sudo dnf install code
+
+# MS Teams: https://github.com/IsmaelMartinez/teams-for-linux
+sudo curl -1sLf -o /tmp/teams-for-linux.asc https://repo.teamsforlinux.de/teams-for-linux.asc
+sudo rpm --import /tmp/teams-for-linux.asc
+sudo rm -f /tmp/teams-for-linux.asc
+sudo curl -1sLf -o /etc/yum.repos.d/teams-for-linux.repo https://repo.teamsforlinux.de/rpm/teams-for-linux.repo
+
+sudo dnf5 install teams-for-linux
+
+# Oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
