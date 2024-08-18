4eefa489f8d60655b8c473a7b071ed7d48c0707a
Dyno Nguyen <tuannguyentn2504@gmail.com>
2024-08-18 14:07:47 +0700

    Update

--- macos/scripts/setup.sh
+++ macos/scripts/setup.sh
@@ -3,5 +3,4 @@
 # Depend on: installation.sh & my-alias.zsh
 
 default_app Code json json5 js ts jsx tsx go html xml css sass scss vue md mdx
-
-rip --graveyard ~/.Trash
+default_app Pixea jpg jpeg png heic heif svg gif ico bmp tiff webp mov mp4 avi mkv
