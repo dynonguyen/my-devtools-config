b5f95b5a59d973677978eda5c73ba614bf7c0587
TuanNLA5 <tuannla5@fpt.com>
2024-08-21 10:33:16 +0700

    Update

--- common/scripts/setup.sh
+++ common/scripts/setup.sh
@@ -7,5 +7,6 @@ git config --global credential.helper store
 git config --global merge.ff false
 git config --global pull.ff yes
 git config --global user.email tuannguyentn2504@gmail.com
+git config --global user.name "Dyno Nguyen"
 git config --global core.excludesFile '~/.gitignore'
 git config --global init.defaultBranch main
